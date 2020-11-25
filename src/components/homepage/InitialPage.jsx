920765952d1c75c8e24afa111c2318d95ea016f6
Natalia Quezada <66387884+nsquezadam@users.noreply.github.com>
2020-11-24 21:17:24 -0300

    arreglo homepage responsive  background y boton down responsive

--- src/components/homepage/InitialPage.jsx
+++ src/components/homepage/InitialPage.jsx
@@ -1,36 +1,36 @@
-import React, { Fragment } from 'react'
-import './InitialPage.css'
-import logo from '../../assets/icon/logoNqm.png'
-import BtnPrincipal from './BtnPrincipal'
+import React, { Fragment } from "react";
+import "./InitialPage.css";
+import logo from "../../assets/icon/logoNqm.svg";
+import BtnPrincipal from "./BtnPrincipal";
+import NavBar from "./Navbar";
 
 const InitialPage = () => {
   return (
     <Fragment>
-    <div className="container_0">
-      <div className="container_1">
-        <div className="container_2">
-          
+      <div className="container_0">
+        <div className="container_1">
+          <div className="container_2"></div>
         </div>
       </div>
-    </div>
-    <div className="body">
-    <div className="containerLogo">
-      <img className="logoImg" src={logo} alt="LogoNqm"/>
-      </div>
-    <div className="titles">
-      <div className="principalTitles">
-      <h1>Natalia Quezada</h1>
-      </div>
-      <div className="profesionTitle">
-      <h2>Front end Developer</h2> 
-      </div>
-      <BtnPrincipal />
-     
-      </div>
-      </div>
-      </Fragment>
-    )
-}
+      <div className="body">
+        <div className="containerLogo">
+          <img className="logoImg" src={logo} alt="LogoNqm" />
+        </div>
 
-export default InitialPage
- 
+        <div className="titles">
+          <div className="principalTitles">
+            <h1>Natalia Quezada</h1>
+          </div>
+          <div className="profesionTitle">
+            <h2>Front end Developer</h2>
+          </div>
+          <BtnPrincipal />
+        </div>
+      </div>
+
+      <NavBar />
+    </Fragment>
+  );
+};
+
+export default InitialPage;
