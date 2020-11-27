0b8acc0a61f7a9339bc1cd28d3582ab6d0b5c314
Natalia Quezada <66387884+nsquezadam@users.noreply.github.com>
2020-11-27 20:54:53 -0300

    cambios homepage responsive  cambio en navBar

---
+++ src/components/about/AboutMe.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../../assets/styles/about/AboutMe.css";
+
+const AboutMe = () => {
+  return (
+    <div className="aboutPage">
+      <h1>Sobre Mi</h1>
+      <p>
+        Natalia Quezada es mi nombre, soy Front end Developer, soy egresada de
+        Laboratoria. Con mucha valentia redirigi mi rumbo a esta hermosa y
+        apasionante profesión, me apasiona enormemente lo que hago. Soy mamá de
+        uno niño de 10 años y un gatuno de 18 años. Llevo mas de 15 años en el
+        mundo laboral trabaje principalmente enfocada a los procesos de venta ,
+        servicio al cliente. Mi compromiso , meta siempre han sido enfocadas a
+        seguir creciendo y desarrollandome como ser humano y profesional.
+      </p>
+    </div>
+  );
+};
+
+export default AboutMe;
