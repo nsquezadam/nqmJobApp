31372382e1d626c454c7702f97b6002de543c513
Natalia Quezada <66387884+nsquezadam@users.noreply.github.com>
2020-12-06 21:26:08 -0300

    revision

--- src/components/proyects/PcomidaParaTodos.jsx
+++ src/components/proyects/PcomidaParaTodos.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../../assets/styles/proyects/pCPT.css";
+// import Slider from "@farbenmeer/react-spring-slider";
 
 const PcomidaParaTodos = () => {
   return <div></div>;
